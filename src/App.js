31bffe234ac191c581d8d8849cd78a73cba2aac7
ssng.kang <ssang.kang@gmail.com>
2024-07-18 13:10:03 +0900

    create react app 소개

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import Button from "./Button";
+import styles from "./App.module.css";
+
 function App() {
-  return <div>Hello Noard React Study</div>;
+  return (
+    <div>
+      <h1 className={styles.title}>Hello Noard React Study</h1>
+      <Button text={"continue"} />
+    </div>
+  );
 }
 
 export default App;
