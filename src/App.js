1e268fc6ecc951dc7413a5bc4a7ead2a136678f7
ssng.kang <ssng.kang@gmail.com>
2024-07-19 12:12:22 +0900

    todolist

--- src/App.js
+++ src/App.js
@@ -1,5 +1,37 @@
+import { useState } from "react";
+
 function App() {
-  return <div>Hello Noard React Study</div>;
+  const [todo, setTodo] = useState("");
+  const [todoList, setTodoList] = useState([]);
+  const onChange = (e) => setTodo(e.target.value);
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (todo === "") {
+      return;
+    }
+    setTodo("");
+    setTodoList((prev) => [todo, ...prev]);
+  };
+  return (
+    <form onSubmit={onSubmit}>
+      <h1>We have todos({todoList.length})</h1>
+      <div>
+        <input
+          onChange={onChange}
+          value={todo}
+          type="text"
+          placeholder="wirte your To Do list..."
+        />
+        <button>Add To Do</button>
+      </div>
+      <hr />
+      <ul>
+        {todoList.map((todo, idx) => (
+          <li key={idx}>{todo}</li>
+        ))}
+      </ul>
+    </form>
+  );
 }
 
 export default App;
